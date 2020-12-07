5e5e2ed7a7405f2c768295976ed1e49ea8fdf463
Michael MAtteis <michaelcmatteis@gmail.com>
2020-12-06 21:28:21 -0600

    adding changes to the static file path in the index.js for herouko

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const package = require('./realServer/controllers/routes/routes/routes/package')
 const order = require("./realServer/controllers/routes/routes/routes/order")
 const cors = require('cors')
 const path = require('path')
+const publicPath = path.join(__dirname,"realClient/client/build")
 const port = process.env.PORT || 4000
+
+
 // seeing if it is working
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname,"realClient/client/build")))
+// app.use(express.static(path.join(__dirname,"realClient/client/build")))
+app.use(express.static(publicPath))
 app.use(package)
 app.use(user)
 app.use(order)
@@ -19,6 +23,10 @@ app.use(cors({
 
 app.get('/', (req, res) => {
     res.send('Welcome to our express app hahahahaha')
+})
+
+app.get('*',(req,res)=>{
+   res.sendFile(path.join(publicPath,"index.html"))
 })
 
 app.listen(port, () => {
